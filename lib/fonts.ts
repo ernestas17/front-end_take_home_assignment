673d3e7213b08ef2c2ca462cd147fa3a39090f8d
Ernestas Rutkauskas <ernesatasrutkauskas@gmail.com>
2025-11-18 06:37:15 +0200

    added fonts and fonts lib

---
+++ lib/fonts.ts
@@ -0,0 +1,23 @@
+// lib/fonts.ts
+import localFont from 'next/font/local';
+import { Roboto } from 'next/font/google';
+
+export const sFProDisplayMedium = localFont({
+  src: '../assets/fonts/sf-pro-display/SF-Pro-Display-Medium.woff2',
+  weight: '600',
+});
+
+export const sFProTextSemibold = localFont({
+  src: '../assets/fonts/sf-pro-display/SF-Pro-Text-Semibold.woff2',
+  weight: '600',
+});
+
+export const sFProTextRegular = localFont({
+  src: '../assets/fonts/sf-pro-display/SF-Pro-Text-Regular.woff2',
+  weight: '400',
+});
+
+export const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
